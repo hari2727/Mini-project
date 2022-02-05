f3b4ed64a200541c50526e3c55f295b0d5508606
hari2727 <hariharankarivarathan@gmail.com>
2022-02-05 11:27:42 +0530

    Browservalidation

---
+++ src/com/sel/mrng/WebBrowser_Validation.java
@@ -0,0 +1,25 @@
+package com.sel.mrng;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebBrowser_Validation {
+	System.setProperty("webdriver.chrome.Driver", "C:\\Users\\best\\eclipse-workspace\\Selenium_Sept\\Driver\\chromedriver.exe");	
+	WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.amazon.in/");
+	driver.manage().window().maximize();
+		String title = driver.getTitle();
+		System.out.println(title);
+		
+		
+		String currentUrl = driver.getCurrentUrl();
+		System.out.println(currentUrl);
+		
+		
+		driver.navigate().to("https://www.flipkart.com/");
+		driver.navigate().back();
+		driver.navigate().forward();
+		driver.close();
+		
+}
